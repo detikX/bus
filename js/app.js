2878787645a2657de7e339c54b08f2d503a00244
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-02-06 15:11:40 +0700

    initial

---
+++ js/app.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panelx");
+
+gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".containerx",
+        pin: true,
+        scrub: 1,
+
+        end: () => "+=" + document.querySelector(".containerx").offsetWidth
+    }
+});
